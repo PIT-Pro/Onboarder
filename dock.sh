c553710d6158eb6d7393e10129761f758929fcca
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-13 15:05:59 +0100

    0.2

---
+++ dock.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# per user settings for PIT Pro clients
+#
+# 2021.12.13 version 1.0 by Thomas Boom, Jeroen Potters | PIT Pro B.V.
+
+# Run script as user
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+
+#replace dock icons (dockutil moet geinstalleerd zijn/in policy staan)
+/usr/local/bin/dockutil --remove "Maps" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "Photos" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "FaceTime" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "Contacts" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "Reminders" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "Music" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "TV" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "Podcasts" /Users/$loggedInUser
+/usr/local/bin/dockutil --add "/Applications/Google Chrome.app" --before "Safari" /Users/$loggedInUser
+/usr/local/bin/dockutil --add "Applications/Microsoft Word"
+/usr/local/bin/dockutil --add "Applications/Microsoft Outlook"
+
+# Don't show recent applications in Dock
+/Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.dock show-recents -bool false
+
+#kill all services
+/Library/Addigy/user-job -user $loggedInUser -run killall ControlCenter
+/Library/Addigy/user-job -user $loggedInUser -run killall Finder
+/Library/Addigy/user-job -user $loggedInUser -run killall systemuiserver
--- menubar.sh
+++ menubar.sh
@@ -1,16 +1,29 @@
-# Show battery percentage in menubar
+#!/bin/bash
+#
+# per user settings for PIT Pro clients
+#
+# 2021.12.13 version 1.0 by Thomas Boom, Jeroen Potters | PIT Pro B.V.
+
+#stopt logged in user in variabele
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+
+# Show battery percentage in menubar (werkt in Big Sur/Monterey)
 /Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-# Tijd en datum
-/Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.menuextra.clock.plist DateFormat -string "$1"
-/Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.menuextra.clock.plist ShowSeconds -bool true
+# Tijd en datum (werkt in Big Sur/Monterey)
+/Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.menuextra.clock "DateFormat" "EEE MMM d h:mm:ss"
 /Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.menuextra.clock.plist ShowDayOfMonth -bool true
-# Show Bluetooth in menubar
-/Library/Addigy/user-job -user $loggedInUser -run defaults write /Users/$loggedInUser/Library/Preferences/ByHost/com.apple.controlcenter.plist Bluetooth -int 18
-# Show Time Machine in menubar
+# Show Bluetooth in menubar (werkt in Big Sur/Monterey)
+/Library/Addigy/user-job -user $loggedInUser -run defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Bluetooth -int 18
+# Show Time Machine in menubar (werkt in Big Sur/Monterey)
 /Library/Addigy/user-job -user $loggedInUser -run open "/System/Library/CoreServices/Menu Extras/TimeMachine.menu"
-# Show Volume in menubar
+# Show Volume in menubar (werkt in Big Sur/Monterey)
  /Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.systemuiserver menuExtras -array-add Volume.menu
- # Show VPN in menubar
+ # Show VPN in menubar (werkt in Big Sur/Monterey)
 /Library/Addigy/user-job -user $loggedInUser -run open "/System/Library/CoreServices/Menu Extras/vpn.menu"
-# Show VPN connected time in menubar
+# Show VPN connected time in menubar (werkt in Big Sur/Monterey)
 /Library/Addigy/user-job -user $loggedInUser -run defaults write com.apple.networkConnect VPNShowTime -string "1"
+
+#kill all services
+/Library/Addigy/user-job -user $loggedInUser -run killall ControlCenter
+/Library/Addigy/user-job -user $loggedInUser -run killall Finder
+/Library/Addigy/user-job -user $loggedInUser -run killall systemuiserver
