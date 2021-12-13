4d463a3ac7e72a7c579def3b34995ceceee445c6
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-13 15:19:51 +0100

    Create main_script.sh

---
+++ main_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# per user settings for PIT Pro clients
+#
+# 2021.12.13 version 1.0 by Thomas Boom, Jeroen Potters | PIT Pro B.V.
+#
+# Run script as user
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+
+source <(curl -s https://github.com/PIT-Pro/Onboarding/blob/main/Production/dock.sh) &
+source <(curl -s https://github.com/PIT-Pro/Onboarding/blob/main/Production/finder.sh) &
+source <(curl -s https://github.com/PIT-Pro/Onboarding/blob/main/Production/menubar.sh)
+
+# Refresh Finder & System Preferences
+/Library/Addigy/user-job -user $loggedInUser -run killall ControlCenter
+/Library/Addigy/user-job -user $loggedInUser -run killall Finder
+/Library/Addigy/user-job -user $loggedInUser -run killall SystemUIServer
