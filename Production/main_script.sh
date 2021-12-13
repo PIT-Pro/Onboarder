e593eb6d2bd730ab2cb86f12b495e361aa3a54dc
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-13 15:20:40 +0100

    0.5

--- Production/main_script.sh
+++ Production/main_script.sh
@@ -7,6 +7,10 @@
 # Run script as user
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
+source <(curl -s https://github.com/PIT-Pro/Onboarding/blob/main/Production/dock.sh) &
+source <(curl -s https://github.com/PIT-Pro/Onboarding/blob/main/Production/finder.sh) &
+source <(curl -s https://github.com/PIT-Pro/Onboarding/blob/main/Production/menubar.sh)
+
 # Refresh Finder & System Preferences
 /Library/Addigy/user-job -user $loggedInUser -run killall ControlCenter
 /Library/Addigy/user-job -user $loggedInUser -run killall Finder
