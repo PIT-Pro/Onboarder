b7c640f04a59114ad130d42324a6a33ff39bfe11
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-13 16:29:24 +0100

    Update main_script.sh

--- Production/main_script.sh
+++ Production/main_script.sh
@@ -2,14 +2,14 @@
 #
 # per user settings for PIT Pro clients
 #
-# 2021.12.13 version 1.0 by Thomas Boom, Jeroen Potters| PIT Pro B.V.
+# 2021.12.13 version 1.0 by Thomas Boom, Jeroen Potters | PIT Pro B.V.
 #
 # Run script as user
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarding/main/Production/dock.sh?token=AWGOUBIIQWLVWKW33OYM2QLBW5P2Q)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarding/main/Production/finder.sh?token=AWGOUBIWUNWUUOZH44KDEIDBW5PWC)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarding/main/Production/menubar.sh?token=AWGOUBP2CKVOBAXELBMWJ2LBW5P3S)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarding/main/Production/dock.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarding/main/Production/finder.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarding/main/Production/menubar.sh)"
 
 # Refresh Finder & System Preferences
 /Library/Addigy/user-job -user $loggedInUser -run killall ControlCenter
