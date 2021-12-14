c3acbcbe16f1196d500d03111b385393807f3c58
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-14 16:12:22 +0100

    added if this then that
    Checks if wallpaper exists, then applies it.

--- Production/1.2/wallpaper.sh
+++ Production/1.2/wallpaper.sh
@@ -4,8 +4,10 @@
 #
 # 2021.12.13 version 1.0 by Thomas Boom | PIT Pro B.V.
 
-#stopt logged in user in variabele
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
-
-#installeert achtergrond die al geupload is naar /User/XXX/Shared
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Shared/wallpaper.png"'
+#check if wallpaper exists, then applies it.
+FILE=/Users/Shared/wallpaper.png
+if [ -f "$FILE" ]; then
+    osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Shared/wallpaper.png"'
+else 
+    echo "$FILE does not exist."
+fi
