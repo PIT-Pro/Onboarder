c26a8be437d7391a44fc544c2b4b0bd15267f462
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-14 15:25:20 +0100

    1.2 update
    wallpaper functionality added

--- Production/1.2/wallpaper.sh
+++ Production/1.2/wallpaper.sh
@@ -7,5 +7,5 @@
 #stopt logged in user in variabele
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-#installeer achtergrond die al geupload is naar /User/x/Shared
+#installeert achtergrond die al geupload is naar /User/XXX/Shared
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Shared/wallpaper.png"'
