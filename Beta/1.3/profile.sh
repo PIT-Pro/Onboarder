97c651203b90fa98e30c3e33d558727d0e580777
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-21 13:40:23 +0100

    Update profile.sh

--- Beta/1.3/profile.sh
+++ Beta/1.3/profile.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-FILE=/Users/Shared/profile.png
-if [ -f "$FILE" ]; then
+
 dscl . delete /Users/$loggedInUser jpegphoto
 dscl . delete /Users/$loggedInUser Picture
 dscl . create /Users/$loggedInUser Picture "/Users/Shared/profile.png"
-else 
-    echo "$FILE does not exist."
-fi
