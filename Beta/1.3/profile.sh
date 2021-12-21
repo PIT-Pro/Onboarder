51fc11c802b705413bfa8b9c3c57690dc8fa35bf
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-21 15:59:56 +0100

    Update profile.sh

--- Beta/1.3/profile.sh
+++ Beta/1.3/profile.sh
@@ -10,9 +10,9 @@ loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConso
 
 FILE=/Users/Shared/profile.png
 if [ -f "$FILE" ] ; then
-dscl . delete /Users/$loggedInUser jpegphoto
-dscl . delete /Users/$loggedInUser Picture
-dscl . create /Users/$loggedInUser Picture "/Users/Shared/profile.png" 
+sudo dscl . delete /Users/$loggedInUser jpegphoto
+sudo dscl . delete /Users/$loggedInUser Picture
+sudo dscl . create /Users/$loggedInUser Picture "/Users/Shared/profile.png" 
 else 
     echo "$FILE does not exist, check if wallpaper file is present in /Users/Shared/"
 fi
