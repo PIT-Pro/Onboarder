8fb6b057e226d9942956951cbfb8b24e3be73ca6
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-21 13:55:17 +0100

    echos added

--- Beta/1.3/Onboarder_1.3.sh
+++ Beta/1.3/Onboarder_1.3.sh
@@ -9,10 +9,15 @@
 # PREREQUISITE: dockutil
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
+echo "DOCK"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/dock.sh)"
+echo "FINDER"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/finder.sh)"
+echo "MENUBAR"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/menubar.sh)"
+echo "WALLPAPER"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/wallpaper.sh)"
+echo "PROFILE PICTURE"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/profile.sh)"
 
 # Refresh Finder & System Preferences
