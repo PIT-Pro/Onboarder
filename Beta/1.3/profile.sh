c3f2736bb7e9f92e7f0e556f31b6f3031f44df6e
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-21 13:39:14 +0100

    Update profile.sh

--- Beta/1.3/profile.sh
+++ Beta/1.3/profile.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-declare -xr AWK_CMD="/usr/bin/awk"
-declare -xr SW_VERS_CMD="/usr/bin/sw_vers"
-declare -xr DSIMPORT_CMD="/usr/bin/dsimport"
-declare -xr ID_CMD="/usr/bin/id"
-
-USERNAME="$1"; export USERNAME
-USERPIC="$2"; export USERPIC
-
-OSVERSION=$(${SW_VERS_CMD} -productVersion | ${AWK_CMD} -F"." '{print $2;exit}'); export OSVERSION
-if [ -f "${USERPIC}" ] ; 
-then
-    if [ "${OSVERSION}" -ge "6" ] ; then
-        declare -x MAPPINGS='0x0A 0x5C 0x3A 0x2C'
-        declare -x ATTRS='dsRecTypeStandard:Users 2 dsAttrTypeStandard:RecordName externalbinary:dsAttrTypeStandard:JPEGPhoto'
-        declare -x PICIMPORT="/Library/Caches/${USERNAME}.picture.dsimport"
-        printf "%s %s \n%s:%s" "${MAPPINGS}" "${ATTRS}" "${USERNAME}" "${USERPIC}" >"${PICIMPORT}"
-        if ${ID_CMD} "${USERNAME}" &>/dev/null ; then
-            ${DSIMPORT_CMD} "${PICIMPORT}" /Local/Default M &&
-            
-    fi
+FILE=/Users/Shared/profile.png
+if [ -f "$FILE" ]; then
+dscl . delete /Users/$loggedInUser jpegphoto
+dscl . delete /Users/$loggedInUser Picture
+dscl . create /Users/$loggedInUser Picture "/Users/Shared/profile.png"
+else 
+    echo "$FILE does not exist."
 fi
