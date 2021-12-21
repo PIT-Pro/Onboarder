b5f0e38682dc0f6aa9b2dc5d59cbb052d26b9649
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-21 12:23:25 +0100

    ff

--- Beta/1.3/Onboarder_1.3.sh
+++ Beta/1.3/Onboarder_1.3.sh
@@ -13,11 +13,9 @@ loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConso
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/finder.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/menubar.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/wallpaper.sh)"
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/profile.sh)" $loggedInUser /User/Shared.profile.png
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/profile.sh)" $loggedInUser /User/Shared.profile.png
 
 # Refresh Finder & System Preferences
 /Library/Addigy/user-job -user $loggedInUser -run killall ControlCenter
 /Library/Addigy/user-job -user $loggedInUser -run killall Finder
 /Library/Addigy/user-job -user $loggedInUser -run killall SystemUIServer
-
-fi
