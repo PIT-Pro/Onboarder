77df73dc50d4478b91a5033d425175c38a2837e6
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-21 15:03:22 +0100

    /dev/null

--- Beta/1.3/Onboarder_1.3.sh
+++ Beta/1.3/Onboarder_1.3.sh
@@ -9,15 +9,15 @@
 # PREREQUISITE: dockutil
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-echo "Removing Apple items and adding Office items in Dock"
+echo "Removing macOS items and adding Office items in Dock"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/dock.sh)"
 echo "Tweaking Finder experience"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/finder.sh)"
 echo "Adding items to Menubar"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/menubar.sh)"
-echo "Changing wallpaper image to Company wallpaper"
+echo "Changing wallpaper image to /Users/Shared/wallpaper.png"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/wallpaper.sh)"
-echo "Changing profile picture to Company logo"
+echo "Changing profile picture to /Users/Shared/profile.png"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/profile.sh)"
 echo "Disabling automatic macOS updates"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Beta/1.3/softwareupdate.sh)"
--- Beta/1.3/dock.sh
+++ Beta/1.3/dock.sh
@@ -9,8 +9,8 @@
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
 #replace dock icons (dockutil moet geinstalleerd zijn/in policy staan)
-/usr/local/bin/dockutil --remove "Maps" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "Photos" /Users/$loggedInUser
+/usr/local/bin/dockutil --remove "Maps" /Users/$loggedInUser 1> /dev/null
+/usr/local/bin/dockutil --remove "Photos" /Users/$loggedInUser 1> /dev/null
 /usr/local/bin/dockutil --remove "FaceTime" /Users/$loggedInUser
 /usr/local/bin/dockutil --remove "Contacts" /Users/$loggedInUser
 /usr/local/bin/dockutil --remove "Reminders" /Users/$loggedInUser
