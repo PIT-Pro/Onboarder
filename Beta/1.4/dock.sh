1684ef7d324f6188331b9553abe1046755d74387
Thomas Boom <thomas.boom@pitpro.nl>
2021-12-27 15:41:42 +0100

    added user-job to printer job

--- Beta/1.4/dock.sh
+++ Beta/1.4/dock.sh
@@ -8,22 +8,22 @@
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
 #replace dock icons (dockutil moet geinstalleerd zijn/in policy staan)
-/usr/local/bin/dockutil --remove "Maps" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "Photos" /Users/$loggedInUser 
-/usr/local/bin/dockutil --remove "FaceTime" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "Contacts" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "Reminders" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "Music" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "TV" /Users/$loggedInUser
-/usr/local/bin/dockutil --remove "Podcasts" /Users/$loggedInUser
-/usr/local/bin/dockutil --add "/Applications/Google Chrome.app" --before "Safari" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "Maps" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "Photos" /Users/$loggedInUser 
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "FaceTime" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "Contacts" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "Reminders" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "Music" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "TV" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove "Podcasts" /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --add "/Applications/Google Chrome.app" --before "Safari" /Users/$loggedInUser
 
 if pgrep Microsoft Word > /dev/null
 then
-    /usr/local/bin/dockutil --add "/Applications/Microsoft Word.app" /Users/$loggedInUser
-    /usr/local/bin/dockutil --add "/Applications/Microsoft Outlook.app" /Users/$loggedInUser
-    /usr/local/bin/dockutil --add "/Applications/Microsoft Excel.app" /Users/$loggedInUser
-    /usr/local/bin/dockutil --add "/Applications/Microsoft Powerpoint.app" /Users/$loggedInUser
+    /Library/Addigy/user-job -user $loggedInUser -run dockutil --add "/Applications/Microsoft Word.app" /Users/$loggedInUser
+    /Library/Addigy/user-job -user $loggedInUser -run dockutil --add "/Applications/Microsoft Outlook.app" /Users/$loggedInUser
+    /Library/Addigy/user-job -user $loggedInUser -run dockutil --add "/Applications/Microsoft Excel.app" /Users/$loggedInUser
+    /Library/Addigy/user-job -user $loggedInUser -run dockutil --add "/Applications/Microsoft Powerpoint.app" /Users/$loggedInUser
 else
     echo "Office not installed"
 fi
--- Beta/1.4/printer.sh
+++ Beta/1.4/printer.sh
@@ -9,4 +9,4 @@
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
 cupsctl WebInterface=yes
-/usr/sbin/lpadmin -p Canon_5355i -P /Library/Printers/PPDs/Contents/Resources/CNMCIRAC5535S2.ppd.gz -v socket://172.16.17.40 -o CNDuplex=DuplexFront -o CNColorMode=mono -o ColorModel=Gray -E 
+/Library/Addigy/user-job -user $loggedInUser -run lpadmin -p Canon_5355i -P /Library/Printers/PPDs/Contents/Resources/CNMCIRAC5535S2.ppd.gz -v socket://172.16.17.40 -o CNDuplex=DuplexFront -o CNColorMode=mono -o ColorModel=Gray -E 
