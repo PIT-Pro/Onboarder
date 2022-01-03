e8b572d29741b949cfa5b804e26e62804787fb24
Thomas Boom <thomas.boom@pitpro.nl>
2022-01-03 12:34:56 +0100

    ' "

--- Production/1.4.2/ENHR/dock.sh
+++ Production/1.4.2/ENHR/dock.sh
@@ -7,23 +7,24 @@
 # Run script as user
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
+killall cfprefsd
 #replace dock icons (dockutil moet geinstalleerd zijn/in policy staan)
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Maps' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Photos' /Users/$loggedInUser 
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'FaceTime' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Contacts' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Reminders' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Music' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'TV' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Podcasts' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Google Chrome.app' --before 'Safari' /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Maps' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Photos' $loggedInUser 
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'FaceTime' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Contacts' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Reminders' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Music' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'TV' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --remove 'Podcasts' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Google Chrome.app' --before 'Safari' $loggedInUser
 
 if pgrep Microsoft Word > /dev/null
 then
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Word.app' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Outlook.app' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Excel.app' /Users/$loggedInUser
-/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Powerpoint.app' /Users/$loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Word.app' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Outlook.app' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Excel.app' $loggedInUser
+/Library/Addigy/user-job -user $loggedInUser -run dockutil --add '/Applications/Microsoft Powerpoint.app' $loggedInUser
 else
     echo "Office not installed"
 fi
