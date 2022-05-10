7568996b5536dcbf5a5e1297278334661d84eca7
Thomas Boom <93121029+PITthomas@users.noreply.github.com>
2022-05-10 14:35:51 +0200

    Update dock.sh

--- Production/1.6/ENHR/dock.sh
+++ Production/1.6/ENHR/dock.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
-#Thanks Tobias Morrison!
-# per user settings for PIT Pro clients
-#
-# 2021.12.21 version 1.6 by Thomas Boom | PIT Pro B.V.
 userjob=/Library/Addigy/user-job
+dockutil=/usr/local/bin/dockutil
 sleep=/bin/sleep
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
  
  #remove all dock icons first
-$userjob -user $loggedInUser -run $dockutil --remove all -user $loggedInUser --no restart
+$userjob -user $loggedInUser -run $dockutil --remove all --no restart
 
-#replace dock icons (dockutil moet geinstalleerd zijn/in policy staan)
-$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/Launchpad.app' -user $loggedInUser --no-restart
-$userjob -user $loggedInUser -run $dockutil --add "/Applications/Google Chrome.app" -user $loggedInUser --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/Applications/Safari.app' -user $loggedInUser --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/System Preferences.app' -user $loggedInUser --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/Applications/Calendar.app' -user $loggedInUser --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/Applications/FaceTime.app' -user $loggedInUser --no-restart
+#replace dock icons ($dockutil moet geinstalleerd zijn/in policy staan)
+$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/Launchpad.app' --no-restart
+$userjob -user $loggedInUser -run $dockutil --add "/Applications/Google Chrome.app" --no-restart
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/Safari.app' --no-restart
+$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/System Preferences.app' --no-restart
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/Calendar.app' --no-restart
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/FaceTime.app' --no-restart
 
 if pgrep -u $loggedInUser Microsoft Word > /dev/null
 then
@@ -41,3 +37,11 @@ $userjob -user $loggedInUser -run defaults write com.apple.dock show-recents -bo
 
 killall Dock
 exit
+
+
+
+
+
+
+
+
