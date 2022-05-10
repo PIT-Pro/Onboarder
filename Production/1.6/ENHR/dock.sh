32942c8469ff6c647fc1e0dc54dbd6eb32ee014a
Thomas Boom <93121029+PITthomas@users.noreply.github.com>
2022-05-10 14:38:48 +0200

    Update dock.sh

--- Production/1.6/ENHR/dock.sh
+++ Production/1.6/ENHR/dock.sh
@@ -4,30 +4,30 @@ sleep=/bin/sleep
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
  
  #remove all dock icons first
-$userjob -user $loggedInUser -run $dockutil --remove all --no restart
+$userjob -user $loggedInUser -run $dockutil --remove all --no restart /Users/$loggedInUser
 
 #replace dock icons ($dockutil moet geinstalleerd zijn/in policy staan)
-$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/Launchpad.app' --no-restart
-$userjob -user $loggedInUser -run $dockutil --add "/Applications/Google Chrome.app" --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/Applications/Safari.app' --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/System Preferences.app' --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/Applications/Calendar.app' --no-restart
-$userjob -user $loggedInUser -run $dockutil --add '/Applications/FaceTime.app' --no-restart
+$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/Launchpad.app' --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/Google Chrome.app' --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/Safari.app' --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add '/System/Applications/System Preferences.app' --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/Calendar.app' --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add '/Applications/FaceTime.app' --no-restart /Users/$loggedInUser
 
 if pgrep -u $loggedInUser Microsoft Word > /dev/null
 then
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Word.app" 
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Outlook.app" 
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Excel.app" 
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Powerpoint.app"
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" 
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Word.app" /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Outlook.app" /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Excel.app" /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Powerpoint.app" /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" /Users/$loggedInUser
 else
     echo "Office not installed"
 fi
 
 if pgrep -u $loggedInUser WhatsApp > /dev/null
 then
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/WhatsApp.app" 
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/WhatsApp.app" /Users/$loggedInUser
 else
     echo "WhatsApp not installed"
 fi
