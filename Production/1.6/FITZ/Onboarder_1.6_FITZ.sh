fa32653643d7cb99b093137a18ccb36c915f9a16
Thomas Boom <93121029+PITthomas@users.noreply.github.com>
2022-06-13 13:40:59 +0200

    added FITZ

---
+++ Production/1.6/FITZ/Onboarder_1.6_FITZ.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# per user settings for PIT Pro clients
+#
+# 2021.12.21 version 1.3 by Thomas Boom | PIT Pro B.V.
+## PREREQUISITE: dockutil, 
+#
+# Run script as user
+
+userjob=/Library/Addigy/user-job
+LOGFILE=/Users/Shared/onboarder_log.txt
+
+if [ -e $LOGFILE ]
+then
+    echo "onboarder_log.txt exists, writing output to file.."
+else
+    echo "onboarder_log.txt does not exist, creating file.."
+fi
+
+echo "START OF SCRIPT" | tee -a $LOGFILE
+echo "Removing macOS items and adding Office items in Dock" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.6/FITZ/dock.sh)" | tee -a $LOGFILE
+echo "Tweaking Finder experience" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.6/FITZ/finder.sh)" | tee -a $LOGFILE
+echo "Adding items to Menubar" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.6/FITZ/menubar.sh)" | tee -a $LOGFILE
+echo "Changing wallpaper image to /Users/Shared/wallpaper.png" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.6/FITZ/wallpaper.sh)" | tee -a $LOGFILE
+echo "Changing profile picture to /Users/Shared/profile.png" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.6/FITZ/profile.sh)" | tee -a $LOGFILE
+echo "Installing Konica Minolta Printer" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.6/FITZ/printer.sh)" | tee -a $LOGFILE
+
+# Refresh Finder & System Preferences
+echo "Refreshing Finder and System Preferences" | tee -a $LOGFILE
+killall ControlCenter  | tee -a $LOGFILE
+killall Finder  | tee -a $LOGFILE
+killall SystemUIServer  | tee -a $LOGFILE
+echo "Done!" | tee -a $LOGFILE
+exit 0
---
+++ Production/1.6/FITZ/dock.sh
@@ -0,0 +1,50 @@
+userjob=/Library/Addigy/user-job
+dockutil=/usr/local/bin/dockutil
+sleep=/bin/sleep
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+ 
+ #remove all dock icons first
+defaults write com.apple.dock persistent-apps -array
+killall Dock
+
+sleep 5
+
+#replace dock icons ($dockutil moet geinstalleerd zijn/in policy staan)
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/Launchpad.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/Applications/Google Chrome.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/Applications/Safari.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/System Preferences.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/Applications/Calendar.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/FaceTime.app" --no-restart /Users/$loggedInUser
+
+if pgrep -u $loggedInUser Microsoft Word > /dev/null
+then
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Word.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Outlook.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Excel.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Powerpoint.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" --no-restart /Users/$loggedInUser
+else
+    echo "Office not installed"
+fi
+
+if pgrep -u $loggedInUser WhatsApp > /dev/null
+then
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/WhatsApp.app" /Users/$loggedInUser
+else
+    echo "WhatsApp not installed"
+fi
+
+# Don't show recent applications in Dock
+$userjob -user $loggedInUser -run defaults write com.apple.dock show-recents -bool false 
+
+killall -KILL Dock
+exit
+
+
+
+
+
+
+
+
