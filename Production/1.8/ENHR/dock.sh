e69a959dc16a253615bbfe99fef4861e9f10b398
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2022-09-20 15:08:41 +0200

    Update dock.sh

--- Production/1.8/ENHR/dock.sh
+++ Production/1.8/ENHR/dock.sh
@@ -12,43 +12,45 @@ sleep 5
 
 #empty dock
 
-$userjob -user "$loggedInUser" -run $dockutil --remove all --no-restart
+$userjob -user $loggedInUser -run $dockutil --remove all --no-restart
 
 #replace dock icons ($dockutil moet geinstalleerd zijn/in policy staan)
-$userjob -user "$loggedInUser" -run $dockutil --add "/System/Applications/Launchpad.app" --no-restart /Users/"$loggedInUser"
-$userjob -user "$loggedInUser" -run $dockutil --add "/System/Applications/Safari.app" --no-restart /Users/"$loggedInUser"
-$userjob -user "$loggedInUser" -run $dockutil --add "/System/Applications/System Preferences.app" --no-restart /Users/"$loggedInUser"
-$userjob -user "$loggedInUser" -run $dockutil --add "/System/Applications/Calendar.app" --no-restart /Users/"$loggedInUser"
-$userjob -user "$loggedInUser" -run $dockutil --add "/System/Applications/FaceTime.app" --no-restart /Users/"$loggedInUser"
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/Launchpad.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/Safari.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/System Preferences.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/Calendar.app" --no-restart /Users/$loggedInUser
+$userjob -user $loggedInUser -run $dockutil --add "/System/Applications/FaceTime.app" --no-restart /Users/$loggedInUser
 
- if [ -d "/Applications/Microsoft Word.app" ]; then
+if [ -d "/Applications/Microsoft Word.app" ]; then
 
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/Microsoft Word.app" --no-restart /Users/"$loggedInUser"
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/Microsoft Outlook.app" --no-restart /Users/"$loggedInUser"
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/Microsoft Excel.app" --no-restart /Users/"$loggedInUser"
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/Microsoft Powerpoint.app" --no-restart /Users/"$loggedInUser"
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/Microsoft Teams.app" --no-restart /Users/"$loggedInUser"
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Word.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Outlook.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Excel.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Powerpoint.app" --no-restart /Users/$loggedInUser
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" --no-restart /Users/$loggedInUser
+
 else
     echo "Office not installed"
 fi
 
- if [ -d "/Applications/Whatsapp.app" ]; then
+if [ -d "/Applications/Whatsapp.app" ]; then
 
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/WhatsApp.app" /Users/"$loggedInUser"
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/WhatsApp.app" /Users/$loggedInUser
+
 else
     echo "WhatsApp not installed"
 fi
 
 if [ -d "/Applications/Google Chrome.app" ]; then
 
-    $userjob -user "$loggedInUser" -run $dockutil --add "/Applications/Google Chrome.app" --no-restart /Users/"$loggedInUser"
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Google Chrome.app" --no-restart /Users/$loggedInUser
 else
     echo "Google Chrome not installed"
 fi
 
 
 # Don't show recent applications in Dock
-$userjob -user "$loggedInUser" -run defaults write com.apple.dock show-recents -bool false 
+$userjob -user $loggedInUser -run defaults write com.apple.dock show-recents -bool false 
 
 killall -KILL Dock
 exit 0
