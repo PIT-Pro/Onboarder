164dfba487f83e39aa083bfb62ab9b032d523540
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2022-09-20 14:46:03 +0200

    new version

---
+++ Production/1.8/SITE/Onboarder_1.8_SITE.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# per user settings for PIT Pro clients
+#
+# 2021.12.21 version 1.3 by Thomas Boom | PIT Pro B.V.
+## PREREQUISITE: dockutil, 
+#
+# Run script as user
+
+userjob=/Library/Addigy/user-job
+LOGFILE=/Users/Shared/onboarder_log.txt
+
+if [ -e $LOGFILE ]
+then
+    echo "onboarder_log.txt exists, writing output to file.."
+else
+    echo "onboarder_log.txt does not exist, creating file.."
+fi
+
+echo "START OF SCRIPT" | tee -a $LOGFILE
+echo "Tweaking Finder experience" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.8/SITE/finder.sh)" | tee -a $LOGFILE
+echo "Adding items to Menubar" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.8/SITE/menubar.sh)" | tee -a $LOGFILE
+echo "Changing wallpaper image to /Users/Shared/wallpaper.png" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.8/SITE/wallpaper.sh)" | tee -a $LOGFILE
+echo "Changing profile picture to /Users/Shared/profile.png" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.8/SITE/profile.sh)" | tee -a $LOGFILE
+echo "Installing Kyocera Printer" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.8/SITE/printer.sh)" | tee -a $LOGFILE
+echo "Configuring dock" | tee -a $LOGFILE
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Onboarder/main/Production/1.8/SITE/dock.sh)" | tee -a $LOGFILE
+
+
+# Refresh Finder & System Preferences
+echo "Refreshing Finder and System Preferences" | tee -a $LOGFILE
+killall ControlCenter  | tee -a $LOGFILE
+killall Finder  | tee -a $LOGFILE
+killall SystemUIServer  | tee -a $LOGFILE
+echo "Done!" | tee -a $LOGFILE
+exit 0
