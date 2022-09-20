06920b4bc67307e73a15fc52a57a0d2dd5dc9464
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2022-09-20 15:25:10 +0200

    Update dock.sh

--- Production/1.8/ENHR/dock.sh
+++ Production/1.8/ENHR/dock.sh
@@ -28,7 +28,6 @@ if [ -d "/Applications/Microsoft Word.app" ]; then
     $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Outlook.app" --no-restart /Users/$loggedInUser
     $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Excel.app" --no-restart /Users/$loggedInUser
     $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Powerpoint.app" --no-restart /Users/$loggedInUser
-    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" --no-restart /Users/$loggedInUser
 
 else
     echo "Office not installed"
@@ -45,8 +44,17 @@ fi
 if [ -d "/Applications/Google Chrome.app" ]; then
 
     $userjob -user $loggedInUser -run $dockutil --add "/Applications/Google Chrome.app" --no-restart /Users/$loggedInUser
+
 else
     echo "Google Chrome not installed"
+fi
+
+if [ -d "/Applications/Microsoft Teams.app" ]; then
+
+    $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" --no-restart /Users/$loggedInUser
+    
+else
+    echo "Microsoft Teams not installed"
 fi
 
 
