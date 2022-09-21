52e958f36ba111542926c5267cdc4b036cc34bf7
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2022-09-21 10:00:46 +0200

    added sleep

--- Production/1.8/ENHR/dock.sh
+++ Production/1.8/ENHR/dock.sh
@@ -8,13 +8,14 @@ loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ &&
  $userjob --user $loggedInUser -run $dockutil --remove all /Users/$loggedInUser --no-restart
 killall Dock
 
-
 #replace dock macOS icons ($dockutil moet geinstalleerd zijn/in policy staan)
 $userjob -user $loggedInUser -run $dockutil --add "/System/Applications/Launchpad.app" --no-restart /Users/$loggedInUser
 $userjob -user $loggedInUser -run $dockutil --add "/Applications/Safari.app" --no-restart /Users/$loggedInUser
 $userjob -user $loggedInUser -run $dockutil --add "/System/Applications/System Preferences.app" --no-restart /Users/$loggedInUser
 $userjob -user $loggedInUser -run $dockutil --add "/System/Applications/Calendar.app" --no-restart /Users/$loggedInUser
 $userjob -user $loggedInUser -run $dockutil --add "/System/Applications/FaceTime.app" --no-restart /Users/$loggedInUser
+
+sleep 5
 
 #add custom software icons
 if [ -d "/Applications/Microsoft Word.app" ]; then
@@ -28,6 +29,8 @@ else
     echo "Office not installed"
 fi
 
+sleep 2
+
 if [ -d "/Applications/Whatsapp.app" ]; then
 
     $userjob -user $loggedInUser -run $dockutil --add "/Applications/WhatsApp.app" /Users/$loggedInUser
@@ -35,6 +38,8 @@ if [ -d "/Applications/Whatsapp.app" ]; then
 else
     echo "WhatsApp not installed"
 fi
+
+sleep 2
 
 if [ -d "/Applications/Google Chrome.app" ]; then
 
@@ -44,6 +49,8 @@ else
     echo "Google Chrome not installed"
 fi
 
+sleep 2
+
 if [ -d "/Applications/Microsoft Teams.app" ]; then
 
     $userjob -user $loggedInUser -run $dockutil --add "/Applications/Microsoft Teams.app" --no-restart /Users/$loggedInUser
@@ -51,6 +58,8 @@ if [ -d "/Applications/Microsoft Teams.app" ]; then
 else
     echo "Microsoft Teams not installed"
 fi
+
+sleep 2
 
 # Don't show recent applications in Dock
 $userjob -user $loggedInUser -run defaults write com.apple.dock show-recents -bool false 
