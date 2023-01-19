ea515f17a9f8569bd91e8139446322b167ae95e0
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2023-01-19 12:01:28 +0100

    added standard user Wifi settings allowed

---
+++ Production/1.8/ENHR/wifi.sh
@@ -0,0 +1,4 @@
+#Allow standard user to remove SSID's from Wifi settings
+/usr/bin/security authorizationdb write system.preferences.network allow
+/usr/bin/security authorizationdb write system.services.systemconfiguration.network allow
+/usr/bin/security authorizationdb write com.apple.wifi allow
