b360fc8bb0f8a26f544db68f612e0701a1023543
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2023-01-19 12:32:59 +0100

    Update wifi.sh

--- Production/1.8/ENHR/wifi.sh
+++ Production/1.8/ENHR/wifi.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 #Allow standard user to remove SSID's from Wifi settings
 /usr/bin/security authorizationdb write system.preferences.network allow
 /usr/bin/security authorizationdb write system.services.systemconfiguration.network allow
 /usr/bin/security authorizationdb write com.apple.wifi allow
+
+#remove to create order
+networksetup -removepreferredwirelessnetwork en0 "EN HR solutions"
+networksetup -removepreferredwirelessnetwork en0 "EN Guest"
 
 #add preferred networks in order
 networksetup -addpreferredwirelessnetworkatindex en0 "EN HR solutions" 0 WPA2E
