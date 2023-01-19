3560d5446b04e9315918eed2109cf2205bf7ad82
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2023-01-19 12:35:01 +0100

    removed delete for onboarding script

--- Production/1.8/ENHR/wifi.sh
+++ Production/1.8/ENHR/wifi.sh
@@ -5,10 +5,6 @@
 /usr/bin/security authorizationdb write system.services.systemconfiguration.network allow
 /usr/bin/security authorizationdb write com.apple.wifi allow
 
-#remove to create order
-networksetup -removepreferredwirelessnetwork en0 "EN HR solutions"
-networksetup -removepreferredwirelessnetwork en0 "EN Guest"
-
 #add preferred networks in order
 networksetup -addpreferredwirelessnetworkatindex en0 "EN HR solutions" 0 WPA2E
 networksetup -addpreferredwirelessnetworkatindex en0 "EN Guest" 1 WPA2
