fb51959efd61910726b467d8e436df8d976b9c2b
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2023-01-23 15:56:22 +0100

    wallpaper working

--- Production/1.8/ADMIN/wallpaper.sh
+++ Production/1.8/ADMIN/wallpaper.sh
@@ -6,8 +6,7 @@
 #
 
 # Run script as user
-userjob=/Library/Addigy/user-job
-wallpaper=/Library/Addigy/PIT\ Pro/wallpaper
+userjob="/Library/Addigy/user-job"
 
 #disable screensaver
 sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime 0
@@ -16,11 +15,12 @@ sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTi
 /bin/bash -c "$(curl -fsSL https://github.com/sindresorhus/macos-wallpaper/releases/download/v2.3.1/wallpaper.zip -o "/Library/Addigy/PIT Pro/wallpaper.zip")" | tee -a $LOGFILE
 
 #Unzip to folder
-unzip "/Library/Addigy/PIT Pro/wallpaper.zip" | tee -a $LOGFILE
+unzip /Library/Addigy/PIT\ Pro/wallpaper.zip -d /Library/Addigy/PIT\ Pro | tee -a $LOGFILE
 
 #set wallpaper to solid red
-$userjob -user localadmin -run $wallpaper set-solid-color C91D22 | tee -a $LOGFILE
+$userjob -user localadmin -run /Library/Addigy/PIT\ Pro/wallpaper set-solid-color C91D22 | tee -a $LOGFILE
 
 #delete zipfile and 'macos-wallpaper'
+sleep 5
 rm -rf "/Library/Addigy/PIT Pro/wallpaper.zip"
 rm -rf "/Library/Addigy/PIT Pro/wallpaper"
