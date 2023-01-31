2d58680263dc36db0d41f653a5bd97c67f4f73ed
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2023-01-31 10:17:25 +0100

    localadmin check included

--- Production/1.9/ADMIN/Onboarder_1.8_ADMIN.sh
+++ Production/1.9/ADMIN/Onboarder_1.8_ADMIN.sh
@@ -2,15 +2,15 @@
 #
 # per user settings for PIT Pro clients
 #
-# 2023.1.24 version 1.8 by Thomas Boom | PIT Pro B.V.
+# 2023.1.31 version 1.9 by Thomas Boom | PIT Pro B.V.
 
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 LOGFILE=/Library/Addigy/PIT\ Pro/onboarder_admin_log.txt
 OSbuild=$(sw_vers -ProductVersion)
 
-# global check if there is a user logged in
-if [ -z "$loggedInUser" -o "$loggedInUser" = "loginwindow" ]; then
-  echo "no user logged in, cannot proceed"
+# global check if localadmin is logged in to prevent wrong settings..
+if [ -z "localadmin" -o "localadmin" = "loginwindow" ]; then
+  echo "localadmin not logged in, cannot proceed"
   exit 1
 fi
 
