f6835549548df7f5634b0425d40c48f1fa7954d3
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2023-08-17 14:43:59 +0200

    changed IP address

--- Production/2.0/KDB/Scripts/printer.sh
+++ Production/2.0/KDB/Scripts/printer.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+#Office Printers
+#HP OfficeJet Pro 8210
+#HP LaserJet MFP M426fdw
+#HP LaserJet Pro M428-429
+
 #Functions
-installPrinter(){
-echo ".ppd will probably be deprecated in macOS 14"
-cupsctl WebInterface=yes
-lpadmin -p Canon_5355i -P /Library/Printers/PPDs/Contents/Resources/CNMCIRAC5535S2.ppd.gz -v socket://172.16.17.40 -o CNDuplex=DuplexFront -o CNColorMode=mono -o ColorModel=Gray -E 
-cupsctl --no-share-printers
+installPrinter1(){
+    echo ".ppd will probably be deprecated in macOS 14"
+    echo "Installing HP OfficeJet Pro 8210.."
+    cupsctl WebInterface=yes
+    lpadmin -p KDB-1 -P "/Library/Printers/PPDs/Contents/Resources/HP OfficeJet Pro 8210.ppd.gz" -v socket://172.16.9.236 -o CNDuplex=DuplexFront -o CNColorMode=mono -o ColorModel=Gray -E 
+    cupsctl --no-share-printers
 }
 
-installPrinter
+installPrinter2(){
+    echo "Installing HP LaserJet MFP M426fdw.."
+    lpadmin -p KDB-2 -P "/Library/Printers/PPDs/Contents/Resources/HP LaserJet Pro MFP M426f-M427f Fax.ppd.gz" -v socket://172.16.9.157 -o CNDuplex=DuplexFront -o CNColorMode=mono -o ColorModel=Gray -E 
+    cupsctl --no-share-printers
+}
+
+installPrinter3(){
+    echo "Installing HP LaserJet Pro M428-429.."
+    lpadmin -p KDB-3 -P "/Library/Printers/PPDs/Contents/Resources/HP LaserJet Pro M428-M429.ppd.gz" -v socket://172.16.9.51 -o CNDuplex=DuplexFront -o CNColorMode=mono -o ColorModel=Gray -E 
+    cupsctl --no-share-printers
+}
+
+installPrinter1
+installPrinter2
+installPrinter3
